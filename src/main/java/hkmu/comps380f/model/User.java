4c5e64e840f342cbd4536815b5f4af11522e7d27
Kirsi Mak <ying987554@hotmail.com>
2024-04-11 01:02:50 +0800

    Create User.java

---
+++ src/main/java/hkmu/comps380f/model/User.java
@@ -0,0 +1,58 @@
+package hkmu.comps380f.model;
+
+public class User {
+    private String id;
+    private String password;
+    private String fullName;
+    private String email;
+    private String address;
+    private String userRole;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
