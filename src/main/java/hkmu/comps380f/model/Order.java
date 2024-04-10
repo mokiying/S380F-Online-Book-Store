979048785e162a9c693f9b6dc1a13e90aa0a8d4b
Kirsi Mak <ying987554@hotmail.com>
2024-04-11 01:39:51 +0800

    Create Order.java
    thinking

---
+++ src/main/java/hkmu/comps380f/model/Order.java
@@ -0,0 +1,15 @@
+package hkmu.comps380f.model;
+
+import java.time.LocalDateTime;
+
+public class Order {
+    private String id;
+    private String userId;
+    private LocalDateTime dateTime;
+    private String state;
+    private int totalNum;
+    private Double totalPrice;
+
+    //{ [bookId, name, author, price, qty] }
+
+}
