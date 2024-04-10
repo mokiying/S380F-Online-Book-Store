daee95eb4f3e899f22b9f106b7bfa4e5e5a69d48
Kirsi Mak <ying987554@hotmail.com>
2024-04-11 01:39:39 +0800

    Update Comment.java

--- src/main/java/hkmu/comps380f/model/Comment.java
+++ src/main/java/hkmu/comps380f/model/Comment.java
@@ -1,7 +1,34 @@
 package hkmu.comps380f.model;
 
 public class Comment {
+    private String id;
+    private String bookId;
+    private String userId;
     private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
     public String getContent() {
         return content;
