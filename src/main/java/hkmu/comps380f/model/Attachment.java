a19a06bc6b46395eab12821feaba4198a5d07405
MickeyTKC <tt51145505@gmail.com>
2024-04-12 18:03:45 +0800

    Display Book

--- src/main/java/hkmu/comps380f/model/Attachment.java
+++ src/main/java/hkmu/comps380f/model/Attachment.java
@@ -1,18 +1,55 @@
 package hkmu.comps380f.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.UUID;
+
+@Entity
 public class Attachment {
-    private String id;
+    @Id
+    @GeneratedValue
+    @ColumnDefault("random_uuid()")
+    private UUID id;
+    @Column(name = "filename")
     private String name;
+    @Column(name = "content_type")
     private String mimeContentType;
+    @Column(name = "content")
+    @Basic(fetch = FetchType.LAZY)
+    @Lob
     private byte[] contents;
+    @Column(name = "book_id", insertable=false, updatable=false)
+    private long bookId;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
 
     // Getters and Setters of id, name, mimeContentType, contents
-    public String getId() {
+
+
+    public UUID getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(UUID id) {
         this.id = id;
+    }
+
+    public long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
     }
 
     public String getName() {
