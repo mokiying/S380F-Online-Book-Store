4ecff9ca68b7f6ae97e8c2cece69c252fe73fd14
MickeyTKC <tt51145505@gmail.com>
2024-04-14 02:32:49 +0800

    Update SQL

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE  IF EXISTS comments;
+DROP TABLE  IF EXISTS comment;
 DROP TABLE IF EXISTS attachment;
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -15,7 +15,7 @@ CREATE TABLE user_roles (
     PRIMARY KEY (user_role_id),
     FOREIGN KEY (username) REFERENCES users(username)
 );
-CREATE TABLE books (
+CREATE TABLE book (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
@@ -30,13 +30,13 @@ CREATE TABLE attachment (
     filename varchar(255),
     book_id INT,
     primary key (id),
-    foreign key (book_id) references books(id)
+    foreign key (book_id) references book(id)
 );
-CREATE TABLE comments (
+CREATE TABLE comment (
      id BIGINT AUTO_INCREMENT PRIMARY KEY,
      username VARCHAR(255),
      content TEXT,
      book_id BIGINT,
-     CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Books(id)
+     CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES book(id)
 );
 
