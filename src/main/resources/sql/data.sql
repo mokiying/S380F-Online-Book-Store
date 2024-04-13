f151b16fbab8f151beaa834e24dc675e127370fb
MickeyTKC <tt51145505@gmail.com>
2024-04-14 02:28:05 +0800

    update SQL Statment

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -8,11 +8,11 @@ INSERT INTO user_roles(username, role) VALUES ('john', 'ROLE_ADMIN');
 INSERT INTO users VALUES ('mary', '{noop}marypw');
 INSERT INTO user_roles(username, role) VALUES ('mary', 'ROLE_USER');
 
-INSERT INTO book (author, availability, description, name, price) VALUES ('Scott E', 10, 'The Model Thinker by Scott E. Page is a guide to thinking about complex problems. It offers practical tools for modeling and analyzing social, economic or political phenomena, and provides insights on how to make better decisions by understanding the world in a more nuanced way.', 'The Model Thinker', 174);
-INSERT INTO book (author, availability, description, name, price) VALUES ('F. Scott Fitzgerald', 5, 'The Model Thinker by Scott E. Page is a guide to thinking about complex problems. It offers practical tools for modeling and analyzing social, economic or political phenomena, and provides insights on how to make better decisions by understanding the world in a more nuanced way.', 'The Great Gatsby', 9);
-INSERT INTO book (author, availability, description, name, price) VALUES ('Jane Austen', 20, 'A beloved English classic, following the lives and loves of the Bennet sisters in 19th century England.', 'Pride and Prejudice', 8.9);
-INSERT INTO book (author, availability, description, name, price) VALUES ('George Orwell', 20, 'A dystopian novel depicting a totalitarian society where individualism and freedom are suppressed.', '1984', 7.9);
-INSERT INTO book (author, availability, description, name, price) VALUES ('Paulo Coelho', 10, 'A philosophical and inspirational tale about a young shepherd following his dreams and discovering the meaning of life.', 'The Alchemist', 11.9);
+INSERT INTO books (author, availability, description, name, price) VALUES ('Scott E', 10, 'The Model Thinker by Scott E. Page is a guide to thinking about complex problems. It offers practical tools for modeling and analyzing social, economic or political phenomena, and provides insights on how to make better decisions by understanding the world in a more nuanced way.', 'The Model Thinker', 174);
+INSERT INTO books (author, availability, description, name, price) VALUES ('F. Scott Fitzgerald', 5, 'The Model Thinker by Scott E. Page is a guide to thinking about complex problems. It offers practical tools for modeling and analyzing social, economic or political phenomena, and provides insights on how to make better decisions by understanding the world in a more nuanced way.', 'The Great Gatsby', 9);
+INSERT INTO books (author, availability, description, name, price) VALUES ('Jane Austen', 20, 'A beloved English classic, following the lives and loves of the Bennet sisters in 19th century England.', 'Pride and Prejudice', 8.9);
+INSERT INTO books (author, availability, description, name, price) VALUES ('George Orwell', 20, 'A dystopian novel depicting a totalitarian society where individualism and freedom are suppressed.', '1984', 7.9);
+INSERT INTO books (author, availability, description, name, price) VALUES ('Paulo Coelho', 10, 'A philosophical and inspirational tale about a young shepherd following his dreams and discovering the meaning of life.', 'The Alchemist', 11.9);
 
-INSERT INTO comment (USERNAME, CONTENT, BOOK_ID) VALUES ('mickey', 'Good Book', 1);
-INSERT INTO comment (USERNAME, CONTENT, BOOK_ID) VALUES ('yelo', 'Bad Book', 1);
+INSERT INTO comments (USERNAME, CONTENT, BOOK_ID) VALUES ('mickey', 'Good Book', 1);
+INSERT INTO comments (USERNAME, CONTENT, BOOK_ID) VALUES ('yelo', 'Bad Book', 1);
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE  IF EXISTS comment;
+DROP TABLE  IF EXISTS comments;
 DROP TABLE IF EXISTS attachment;
-DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -15,7 +15,7 @@ CREATE TABLE user_roles (
     PRIMARY KEY (user_role_id),
     FOREIGN KEY (username) REFERENCES users(username)
 );
-CREATE TABLE book (
+CREATE TABLE books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
@@ -30,13 +30,13 @@ CREATE TABLE attachment (
     filename varchar(255),
     book_id INT,
     primary key (id),
-    foreign key (book_id) references book(id)
+    foreign key (book_id) references books(id)
 );
-CREATE TABLE comment (
+CREATE TABLE comments (
      id BIGINT AUTO_INCREMENT PRIMARY KEY,
      username VARCHAR(255),
      content TEXT,
      book_id BIGINT,
-     CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Book(id)
+     CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Books(id)
 );
 
