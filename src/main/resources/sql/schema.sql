91eceec302f7609f9a3c05ce1cba8ee69a9b4d5d
MickeyTKC <tt51145505@gmail.com>
2024-04-13 16:07:41 +0800

    Update Build in Data

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS attachment;
+DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -20,22 +20,14 @@ CREATE TABLE book (
     author VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     description VARCHAR(500),
-    available BOOLEAN
+    availability INTEGER
 );
 CREATE TABLE attachment (
     id uuid default random_uuid() not null,
     content blob,
     content_type varchar(255),
     filename varchar(255),
-    ticket_id bigint,
+    book_id INT,
     primary key (id),
     foreign key (book_id) references book(id)
-);
-CREATE TABLE comments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  book_id bigint NOT NULL,
-  username VARCHAR(50) NOT NULL,
-  comment_text VARCHAR(255),
-  FOREIGN KEY (book_id) REFERENCES books (id),
-  FOREIGN KEY (username) REFERENCES users (username)
 );
