3756225c9263b4988aa28a106157d16b80a30506
MickeyTKC <tt51145505@gmail.com>
2024-04-13 18:11:46 +0800

    update Book Exception Message

--- src/main/java/hkmu/comps380f/exception/BookNotFound.java
+++ src/main/java/hkmu/comps380f/exception/BookNotFound.java
@@ -2,6 +2,6 @@ package hkmu.comps380f.exception;
 
 public class BookNotFound extends  Exception{
     public BookNotFound(long id) {
-        super("Ticket " + id + " does not exist.");
+        super("Book " + id + " does not exist.");
     }
 }
