d683c5dae5cb71a081d49587c5a044acf3ffe1e0
Kirsi Mak <ying987554@hotmail.com>
2024-04-14 19:25:47 +0800

    create user
    not finish, no cart
    UI null

--- src/main/java/hkmu/comps380f/controller/UserController.java
+++ src/main/java/hkmu/comps380f/controller/UserController.java
@@ -3,17 +3,24 @@ package hkmu.comps380f.controller;
 import hkmu.comps380f.dao.Service.UserManagementService;
 import hkmu.comps380f.exception.UserNotFound;
 import hkmu.comps380f.model.BookUser;
+import hkmu.comps380f.model.Comment;
+import hkmu.comps380f.model.UserRole;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.servlet.View;
 
+import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -28,6 +35,78 @@ public class UserController {
         return "userList";
     }
 
+    @GetMapping("/create")
+    public ModelAndView create() {
+        return new ModelAndView("add", "userForm", new UserController.Form());
+    }
+    public static class Form {
+        private String username;
+        private String password;
+        private String fullName;
+        private String email;
+        private String address;
+        private String[] roles;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String[] getRoles() {
+            return roles;
+        }
+
+        public void setRoles(String[] roles) {
+            this.roles = roles;
+        }
+    }
+    @PostMapping("/create")
+    public View create(Form form) throws IOException {
+        String username = uService.createUser(
+                form.getUsername(),
+                form.getPassword(),
+                form.getFullName(),
+                form.getEmail(),
+                form.getAddress(),
+                form.getRoles()
+        );
+        return new RedirectView("/user/detail/" + username, true);
+    }
     @GetMapping(value = {"", "/detail/{username}"})
     public String view(ModelMap model,
                        @PathVariable("username") String username) throws UserNotFound {
